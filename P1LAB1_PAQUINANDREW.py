35d32c33de35efe401e0256da513a2fffbeb1e0f
ampaquin37 <paquina8900@student.faytechcc.edu>
2026-09-13 18:45:02 -0400

    Add files via upload

---
+++ P1LAB1_PAQUINANDREW.py
@@ -0,0 +1,9 @@
+# Andrew Paquin
+# 13 SEPTEMBER 2026
+# P1LAB1
+# Introduction assignment into Python language
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,",first_name,last_name +"!","Welcome to CTI-110!")
