40ac2517b770572e5ab35d8ded74211400e059ac
ampaquin37 <paquina8900@student.faytechcc.edu>
2026-09-13 19:36:36 -0400

    Add files via upload

---
+++ P1HW1_PAQUINANDREW.py
@@ -0,0 +1,25 @@
+# Andrew Paquin
+# 13 SEPTEMBER 2026
+# P1WHW1
+# Calculate exponenets and addition and subtraction
+
+# calculate exponenets
+print("----------Exponents----------")
+print('\v')
+base = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+result = base ** exponent
+print('\v')
+print(base,"raised to the power of",exponent,"is",result,"!!")
+
+print('\v')
+
+# calculate addition and subtraction
+print("----------Addition and Subtraction----------")
+print('\v')
+num1 = int(input("Enter a starting integer: ")) 
+num2 = int(input("Enter an integer to add: "))
+num3 = int(input("Enter an integer to subtract: "))
+result= int(num1 + num2 - num3)
+print('\v')
+print(num1,"+",num2,"-",num3,"is equal to",result)
